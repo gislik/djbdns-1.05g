50f02e50822e93fef18f1cb7cbcd5c9ec391df42
Gísli Kristjánsson <gislik@hamstur.is>
2014-05-12 13:17:51 +0000

    Only return the dlenp if the pointer is not NULL

--- dns_dwlk.c
+++ dns_dwlk.c
@@ -5,7 +5,7 @@ inline int dns_domain_walk(char **dp, unsigned int *dlenp)
   unsigned int n;
   if (!**dp) return 0;
   n = 1 + (unsigned int) **dp;
-  *dlenp -= n;
+  if (dlenp) *dlenp -= n;
   *dp += n;
   return 1;
 }
